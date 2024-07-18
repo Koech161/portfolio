736467093ab8e19537ce1ecc7d0cd727e255a059
Abel Koech <koechabel161@gmail.com>
2024-07-18 20:33:35 +0300

    final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cvBtn=document.getElementById('resume-btn')
 const hirebtn=document.getElementById('hire')
-const navbar=document.getElementById('nav')
-const projects=document.getElementById('cv')
+
+
 
 cvBtn.addEventListener('click',findCv)
 function findCv(){
