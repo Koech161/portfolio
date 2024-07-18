dd6a7316ca3923faa91f8a3b266d4092aedfa8f0
Abel Koech <koechabel161@gmail.com>
2024-07-18 19:01:53 +0300

    functions

---
+++ index.js
@@ -0,0 +1,9 @@
+const cvBtn=document.getElementById('resume-btn')
+const hirebtn=document.getElementById('hire')
+const navbar=document.getElementById('nav')
+const projects=document.getElementById('cv')
+
+cvBtn.addEventListener('click',findCv)
+function findCv(){
+return //projects.textContent=`https://koech161.github.io/week-2-code-challenge/`
+}findCv()
