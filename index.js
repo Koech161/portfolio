4d8a752af04c3e015fac2ead8bb7d1700e30b32a
Abel Koech <koechabel161@gmail.com>
2024-07-26 15:40:45 +0300

    adding dark theme

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const cvBtn=document.getElementById('resume-btn')
-const hirebtn=document.getElementById('hire')
+// const cvBtn=document.getElementById('resume-btn')
+// const hirebtn=document.getElementById('hire')
 
 
 
-cvBtn.addEventListener('click',findCv)
-function findCv(){
-return //projects.textContent=`https://koech161.github.io/week-2-code-challenge/`
-}findCv()
+// cvBtn.addEventListener('click',findCv)
+// function findCv(){
+// return //projects.textContent=`https://koech161.github.io/week-2-code-challenge/`
+// }findCv()
+
+
+const icon= document.getElementById('icon')
+
+icon.onclick= function(){
+    document.body.classList.toggle('dark-theme')
+
+    if(document.body.classList.contains('dark-theme')){
+        icon.src="./sun.png"
+    }else{
+        icon.src="./moon.png"
+    }
+}
